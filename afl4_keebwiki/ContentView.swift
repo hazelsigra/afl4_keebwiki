201a432bc34f08bac6ae5ac4669f380aef312b4b
Hazel <84302030+hazelsigra@users.noreply.github.com>
2022-06-06 22:02:49 +0700

    Add files via upload

---
+++ afl4_keebwiki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  afl4_keebwiki
+//
+//  Created by MacBook Pro on 06/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, test!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ afl4_keebwiki/afl4_keebwikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  afl4_keebwikiApp.swift
+//  afl4_keebwiki
+//
+//  Created by MacBook Pro on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct afl4_keebwikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
